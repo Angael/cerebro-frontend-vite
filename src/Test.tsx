1d758cde4d64464add893fc87d23f42ad2a58065
Angael <krzysztofwidacki@gmail.com>
2023-02-18 14:43:45 +0100

    init

---
+++ src/Test.tsx
@@ -0,0 +1,12 @@
+import css from './Test.module.scss'
+
+function Test() {
+
+  return (
+    <div className={css.dupa}>
+        Dupa Test
+    </div>
+  )
+}
+
+export default Test
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig, UserConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 4000
+  }
+})

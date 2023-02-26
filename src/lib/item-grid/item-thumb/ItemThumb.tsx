8afe159c15d13b5bc2924c587d7d6c67277b9599
Angael <krzysztofwidacki@gmail.com>
2023-02-26 21:43:13 +0100

    icons fix for prod build

--- src/lib/item-grid/item-thumb/ItemThumb.tsx
+++ src/lib/item-grid/item-thumb/ItemThumb.tsx
@@ -4,7 +4,7 @@ import ProgressiveImage from 'react-progressive-graceful-image';
 import css from './ItemThumb.module.scss';
 import { getGridSpan } from './getGridSpan';
 import { mdiAlertCircleOutline, mdiClockOutline } from '@mdi/js';
-import Icon from '@mdi/react';
+import { Icon } from '@mdi/react';
 import { FrontItem } from '@vanih/cerebro-contracts';
 import clsx from 'clsx';
 import { Link } from 'react-router-dom';
--- src/lib/loaders/CircleLoader.tsx
+++ src/lib/loaders/CircleLoader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Icon from '@mdi/react';
+import { Icon } from '@mdi/react';
 import { mdiLoading } from '@mdi/js';
 import css from './CircleLoader.module.scss';
 import clsx from 'clsx';

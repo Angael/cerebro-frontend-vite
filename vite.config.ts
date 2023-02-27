5797beb9d5667c39790f7fb094158cba604248a1
Angael <krzysztofwidacki@gmail.com>
2023-02-27 22:44:11 +0100

    vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
+    proxy: {
+      '/vite-proxy': {
+        target: 'http://192.168.1.100:3000',
+        rewrite: (path) => path.replace('/vite-proxy', ''),
+      },
+    },
   },
   css: {
     modules: {

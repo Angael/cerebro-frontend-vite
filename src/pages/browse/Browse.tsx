7a7c9e1ce44ab7fb856148578e43f6b76ddfcc23
Angael <krzysztofwidacki@gmail.com>
2023-04-15 12:17:01 +0200

    walls selection

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -8,16 +8,20 @@ import Pagination from './Pagination';
 import { PAGINATION_LIMIT } from '../../utils/consts';
 import CircleLoader from '../../lib/loaders/CircleLoader';
 import IconWithText from '../../lib/icon-with-text/IconWithText';
+import WallSelectInput from './WallSelectInput';
 
 const Browse = () => {
   const outlet = useOutlet();
 
   const [page, setPage] = useState(0);
+  // TODO: Change to tags
+  const [wall, setWall] = useState<string>('');
   const items = useQueryItems(!outlet, PAGINATION_LIMIT, page);
 
   return (
     <Layout isMaxWidth>
       {outlet}
+      <WallSelectInput wall={wall} setWall={setWall} />
       <Pagination items={items.data} page={page} setCursor={setPage} />
       <div>{items.data && <ItemGrid items={items.data} />}</div>
       {items.isError && items.data.length === 0 && (

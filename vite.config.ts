a3881ddf9289129caa29a603fefdc5ce7f180ee0
Angael <krzysztofwidacki@gmail.com>
2023-04-16 12:49:20 +0200

    Add analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig, PluginOption, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
+// Probably not a dev dependency, it's build in ci/cd
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.ANALYZE === 'true' &&
+      (visualizer({
+        template: 'treemap', // or sunburst
+        open: true,
+        gzipSize: true,
+        brotliSize: false,
+        filename: 'dist/analize.html',
+      }) as PluginOption),
+  ],
   server: {
     port: 4000,
     proxy: {

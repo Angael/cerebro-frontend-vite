f2b9a745fd8341aff14afbd52a4e67684fbaf7ed
Angael <krzysztofwidacki@gmail.com>
2023-06-14 21:16:40 +0200

    tags editor modal; api endpoint

--- src/store/browse/editTagsStore.ts
+++ src/store/browse/editTagsStore.ts
@@ -6,7 +6,6 @@ type EditTagsStore = {
 };
 
 export const useEditTags = create<EditTagsStore>((set) => ({
-  // in case of performance issues, use a Set instead of an array
   opened: false,
   setOpen: (opened: boolean) => set({ opened }),
 }));

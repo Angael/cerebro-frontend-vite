66d90021724e88bc84d0df8a325a14e429c7792c
Angael <Krzysztofwidacki@gmail.com>
2023-07-23 11:40:20 +0200

    react-dropzone; FilePreview dropzone; onDrop animation (#22)
    * react-dropzone; FilePreview dropzone; onDrop animation
    
    * no loading div

--- src/lib/import/files-preview/FilesPreview.tsx
+++ src/lib/import/files-preview/FilesPreview.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import FilePreview from './FilePreview';
 import css from './FilesPreview.module.scss';
 import { ExtendedFile } from '../../../store/upload/uploadTypes';
+import { useDropzone } from 'react-dropzone';
+import { styled } from '../../../styled/styled';
+import { mdiFileMultiple, mdiImage, mdiPaperclip, mdiUpload } from '@mdi/js';
+import { Icon } from '@mdi/react';
 
 interface IProps {
   files: ExtendedFile[];
   onDelete: (id: string) => void;
+  onAddFiles: (files: File[]) => void;
 }
 
-const FilesPreview = ({ files, onDelete }: IProps) => {
+const Grid = styled('section', css.MyGrid);
+
+const FilesPreview = ({ files, onDelete, onAddFiles }: IProps) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: onAddFiles,
+    noClick: true,
+  });
+
   return (
-    <div>
-      <div className={css.MyGrid}>
-        {files.map((file) => (
-          <FilePreview file={file} key={file.id} onDelete={onDelete} />
-        ))}
-      </div>
-    </div>
+    <Grid {...getRootProps()} className={isDragActive ? css.onDrag : undefined}>
+      {files.length === 0 && (
+        <div className={css.dragWatermark}>
+          <Icon path={mdiFileMultiple} size={3} />
+          <p>Drag & drop files</p>
+        </div>
+      )}
+      {files.map((file) => (
+        <FilePreview file={file} key={file.id} onDelete={onDelete} />
+      ))}
+    </Grid>
   );
 };
 
-export default FilesPreview;
+export default React.memo(FilesPreview);

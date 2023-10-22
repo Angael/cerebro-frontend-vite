5a276be1d565323508e12a28621fedac9bdf016c
Angael <krzysztofwidacki@gmail.com>
2023-10-22 15:08:32 +0200

    Initial json pre preview on useQuery for import from link

--- src/lib/import/import-from-link/StatsFromLink.tsx
+++ src/lib/import/import-from-link/StatsFromLink.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import css from './StatsFromLink.module.scss';
+import Card from '../../../styled/card/Card';
+import { isProd } from '../../../env';
 
 type Props = {
   stats: any;
 };
 
-const StatsFromLink = (props: Props) => {
+const StatsFromLink = ({ stats }: Props) => {
+  const { title, description, duration, thumbnail, resolution, fps, ext } =
+    stats;
+
+  if (!isProd) {
+    console.log(stats);
+  }
+
   return (
-    <div>
-      <header>Nazwa filmu</header>
-      <p>Description</p>
-      <p>Length</p>
-      <pre>
-        <code>{JSON.stringify(props.stats, null, 2)}</code>
-      </pre>
-    </div>
+    <Card>
+      <img src={thumbnail} alt='thumbnail' />
+      <header className='h3'>Nazwa filmu: {title}</header>
+      <p className='body2'>Description: {description}</p>
+      <p className='body2'>Duration: {duration}</p>
+      <p className='body2'>Resolution: {resolution}</p>
+      <p className='body2'>FPS: {fps}</p>
+      <p className='body2'>Ext: {ext}</p>
+    </Card>
   );
 };
 

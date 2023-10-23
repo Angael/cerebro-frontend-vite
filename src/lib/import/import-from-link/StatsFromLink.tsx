931994c8a867821608be58cc33f3aea2ccdba12c
Angael <krzysztofwidacki@gmail.com>
2023-10-23 23:28:10 +0200

    StatsFromLink and format stuff, aspect ratio for image, breakpoints

--- src/lib/import/import-from-link/StatsFromLink.tsx
+++ src/lib/import/import-from-link/StatsFromLink.tsx
@@ -1,42 +1,38 @@
 import React from 'react';
+import numeral from 'numeral';
+
 import css from './StatsFromLink.module.scss';
 import Card from '../../../styled/card/Card';
 import { isProd } from '../../../env';
 
 type Props = {
+  // TODO: type
   stats: any;
 };
 
 const StatsFromLink = ({ stats }: Props) => {
-  const {
-    title,
-    description,
-    duration,
-    thumbnail,
-    resolution,
-    fps,
-    ext,
-    filesize_approx,
-  } = stats;
+  const { title, duration, thumbnail, resolution, fps, ext, filesize_approx } =
+    stats;
 
   if (!isProd) {
     console.log(stats);
   }
+
+  const sizeStr = numeral(filesize_approx).format('0.00 b');
+  const durationStr = numeral(duration).format('00:00:00');
 
   return (
     <Card className={css.StatsFromLink}>
       <img src={thumbnail} alt='thumbnail' className={css.thumbnail} />
       <div className={css.content}>
         <header className={'h3 ' + css.title}>{title}</header>
-        <div className='desc'>
-          {description && <p className='body2'>{description}</p>}
-          <p className='body2'>Duration: {duration}s</p>
-          <p className='body2'>Resolution: {resolution}</p>
-          <p className='body2'>FPS: {fps}</p>
-          <p className='body2'>Ext: {ext}</p>
-          <p className='body2'>
-            Filesize: {Math.round(filesize_approx / 1000000)}MB
-          </p>
+
+        <div className={css.stats}>
+          <p>Duration: {durationStr}s</p>
+          <p>Resolution: {resolution}</p>
+          <p>FPS: {fps}</p>
+          <p>Ext: {ext}</p>
+          <p>Filesize: {sizeStr}</p>
         </div>
       </div>
     </Card>
